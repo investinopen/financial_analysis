672aae7e7cf7be229f120abf2176bdf9b6f828cb
Tania Hernandez <tania@investinopen.org>
2022-08-10 14:18:48 -0500

    Updated August 10

--- Calculations.R
+++ Calculations.R
@@ -52,52 +52,53 @@ data1 <- read.csv("C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastruct
 data2 <- read.csv("C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastructure/R project/Datasets with calculations/CExpenses2010.csv")
 data3 <- read.csv("C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastructure/R project/Filtered cases/FBalance2010.csv")
 
-#merging the three datasets
-df_list <- list(data1, data2, data3) 
-data2010 <- df_list %>% reduce(full_join, by='ORG_EIN')
+#only select the varaibles of interest in each dataset
+data1a <- data1[,c("ORG_EIN", "ORG_NAME_L1", "RETURN_TYPE", "TAX_YEAR", "revcont", "F9_08_REV_CONTR_GOVT_GRANT", "F9_08_REV_PROG_TOT_TOT", "revinvest", "revse", "F9_08_REV_OTH_INV_NET_TOT", "F9_08_REV_OTH_INV_NET_TOT", "revother", "F9_08_REV_TOT_TOT")]
+data2a <- data2[,c("ORG_EIN", "F9_09_EXP_TOT_PROG", "F9_09_EXP_TOT_MGMT", "F9_09_EXP_TOT_FUNDR", "expper", "F9_09_EXP_TOT_TOT")] 
+data3a <- data3[,c("ORG_EIN", "F9_10_ASSET_CASH_EOY", "F9_10_ASSET_TOT_EOY", "F9_10_LIAB_TOT_EOY")]
 
-#only selecting varibles of interest
-data2010c <- data2010[,c("ORG_EIN", "RETURN_VERSION", "ORG_NAME_L1", "RETURN_TYPE", "TAX_YEAR", "revcont", "F9_08_REV_CONTR_GOVT_GRANT", "F9_08_REV_PROG_TOT_TOT", "revinvest", "revse", "REV_OTH_INV_NET_TOT", "revother", "F9_08_REV_TOT_TOT", "F9_09_EXP_TOT_PROG", "F9_09_EXP_TOT_MGMT", "F9_09_EXP_TOT_FUNDR", "expper","F9_09_EXP_TOT_TOT", "F9_10_ASSET_CASH_EOY", "F9_10_ASSET_TOT_EOY", "F9_10_LIAB_TOT_EOY")]
+#merging the three datasets
+df_list <- list(data1a, data2a, data3a) 
+dataall <- df_list %>% reduce(full_join, by='ORG_EIN')
+
 
 #calculations for ratios
 
 # Ratio 1. Days cash on hand
-data2010c$ratio1 <- data2010c$F9_09_ASSET_CASH_EOY/(data2010c$F9_09_EXP_TOT_TOT/365)
+dataall$ratio1 <- dataall$F9_10_ASSET_CASH_EOY/(dataall$F9_09_EXP_TOT_TOT/365)
 
-# Ratio 2. Leverage ratio
-data2010c$ratio2 <-data2010c$F9_10_LIAB_TOT_EOY/data2010c$F9_10_ASSET_TOT_EOY
+# Ratio 2. Leverage ratio WORKED!
+dataall$ratio2 <-dataall$F9_10_LIAB_TOT_EOY/dataall$F9_10_ASSET_TOT_EOY
 
-# Ratio 3. Reliance on a revenue source ratio
+# Ratio 3. Reliance on a revenue source ratio WORKED! 
+dataall$largestrev <- pmax(dataall$revcont, dataall$F9_08_REV_CONTR_GOVT_GRANT, dataall$F9_08_REV_PROG_TOT_TOT, dataall$revinvest, dataall$revse, dataall$F9_08_REV_OTH_INV_NET_TOT, dataall$revother)
+dataall$ratio3 <-dataall$largestrev/dataall$F9_08_REV_TOT_TOT 
 
-# "revcont" "F9_08_REV_CONTR_GOVT_GRANT" "F9_08_REV_PROG_TOT_TOT" "revinvest" "revse" "F9_08_REV_OTH_INV_NET_TOT" "revother"
-data2010c$largestincome <- pmax(data2010c$revcont, data2010c$F9_08_REV_CONTR_GOVT_GRANT, data2010c$F9_08_REV_PROG_TOT_TOT, data2010c$revinvest, data2010c$revse, data2010c$F9_08_REV_OTH_INV_NET_TOT, data2010c$revother)
-data2010c$ratio3 <-data2010c$largestincome/data2010c$F9_08_REV_TOT_TOT 
+# Ratio 4. Government reliance ratio WORKED!
+dataall$ratio4 <-dataall$F9_08_REV_CONTR_GOVT_GRANT/dataall$F9_08_REV_TOT_TOT 
 
-# Ratio 4. Government reliance ratio
-data2010c$ratio4 <-data2010c$F9_08_REV_CONTR_GOVT_GRANT/data2010c$F9_08_REV_TOT_TOT 
+# Ratio 5. Contributions reliance ratio WORKED!
+dataall$ratio5 <-dataall$revcont/dataall$F9_08_REV_TOT_TOT 
 
-# Ratio 5. Contributions reliance ratio
-data2010c$ratio5 <-data2010c$revcont/data2010c$F9_08_REV_TOT_TOT 
-
-# Ratio 6. Programme service revenue reliance  
-data2010c$ratio6 <-data2010c$F9_08_REV_PROG_TOT_TOT/data2010c$F9_08_REV_TOT_TOT
+# Ratio 6. Programme service revenue reliance WORKED!  
+dataall$ratio6 <-dataall$F9_08_REV_PROG_TOT_TOT/dataall$F9_08_REV_TOT_TOT
 
 # Ratio 7. Programme expense ratio 
-data2010c$ratio7 <-data2010c$F9_09_EXP_TOT_PROG/data2010c$F9_09_EXP_TOT_TOT
+dataall$ratio7 <-dataall$F9_09_EXP_TOT_PROG/dataall$F9_09_EXP_TOT_TOT
 
 # Ratio 8. Administrative expense ratio
-data2010c$ratio8 <-data2010c$F9_09_EXP_TOT_MGMT/data2010c$F9_09_EXP_TOT_TOT
+dataall$ratio8 <-dataall$F9_09_EXP_TOT_MGMT/dataall$F9_09_EXP_TOT_TOT
 
 # Ratio 9. Fundraising expense ratio
-data2010c$ratio9 <-data2010c$F9_09_EXP_TOT_FUNDR/data2010c$F9_09_EXP_TOT_TOT
+dataall$ratio9 <-dataall$F9_09_EXP_TOT_FUNDR/dataall$F9_09_EXP_TOT_TOT
 
-# Ratio 10. Personnel expense ratio  
-data2010c$ratio10 <-data2010c$revcont/data2010c$F9_08_REV_TOT_TOT
+# Ratio 10. Personnel expense ratio WORKED!  
+dataall$ratio10 <-dataall$revcont/dataall$F9_08_REV_TOT_TOT
     
 #save table
-write.csv(data2010c,"C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastructure/R project/Datasets with ratios/Data2010.csv", row.names = FALSE)
+write.csv(dataall,"C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastructure/R project/Datasets with ratios/Data2010.csv", row.names = FALSE)
 
 #delete data
-rm(data2010)
-rm(data2010c)
+rm(dataall)
+
 
