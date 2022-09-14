f94469972b72b56116ec5649a02ebd854ca8e481
Richard Dunks <richarddunks@gmail.com>
2022-09-14 12:08:59 -0500

    adding example working code for string interpolation

---
+++ string_interpolation_ex.r
@@ -0,0 +1,5 @@
+str_intr(i, "C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastructure/R project/Datasets per organization transposed/'i'.csv"")
+
+library(stringr)
+i <- '521447747'
+str_interp("C:/Users/tatis/Dropbox (ASU)/0000. Invest in Open Infrastructure/R project/Datasets per organization transposed/${i}.csv")
